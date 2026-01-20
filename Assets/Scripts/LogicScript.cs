4c2931d61957f8c79f93234c6b59eefe2c35c808
emmag37 <emmaglenn1@gmail.com>
2026-01-20 14:17:31 -0500

    added title screen

--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -9,9 +9,35 @@ public class LogicScript : MonoBehaviour
     public int playerScore = 0;
     public Text scoreText;
     public GameObject gameOverScreen;
+    public GameObject startScreen;
     public AudioSource dingSFX;
 
     public bool over = false;
+    public static bool skipStart = false;
+
+    void Start()
+    {
+        if (skipStart)
+        {
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            // Open game with the start screen
+            startScreen.SetActive(true);
+
+            // "pause" all game logic
+            Time.timeScale = 0f;
+        }
+
+    }
+
+    public void startGame()
+    {
+        startScreen.SetActive(false);
+
+        Time.timeScale = 1;   // unpause
+    }
 
     [ContextMenu("Increase Score")]     // can now use while unity is running
     public void addScore(int scoreToAdd)
@@ -23,11 +49,12 @@ public class LogicScript : MonoBehaviour
 
     public void restartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // this resets everything, including the bool that you are using to turn off the startscreen
     }
 
     public void gameOver()
     {
+        skipStart = true;
         gameOverScreen.SetActive(true); 
     }
 
